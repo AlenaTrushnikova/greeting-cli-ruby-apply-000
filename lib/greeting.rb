e5467abeb249f5bd48b600adbbb9ad0c9d09af76
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-09 23:31:42 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,10 +1,2 @@
 # code the #greeting method here!
 
-#!/usr/bin/env ruby
-
-require_relative '../lib/greeting.rb'
-
-# puts "Hi! Welcome to the wonderful world of Ruby programming."
-# puts "Please enter your name so that we can greet you more personally:"
-# name = gets.strip
-# greeting(name)
